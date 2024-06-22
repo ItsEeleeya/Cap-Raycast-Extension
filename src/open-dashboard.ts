ea2f8de0587b94e72be57d510ce309fa288cd95e
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-06-23 00:04:23 +0330

    Update open-dashboard.ts

--- src/open-dashboard.ts
+++ src/open-dashboard.ts
@@ -1,11 +1,5 @@
 import { open } from "@raycast/api";
-import { capNotInstalled, launchCap } from "./utils";
 
 export default async function Command() {
-  if (await capNotInstalled(false)) {
-    open("https://cap.so/dashboard/");
-    return;
-  }
-
-  await launchCap("open-dashboard");
+  open("https://cap.so/dashboard/");
 }
