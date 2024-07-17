4cf413176acb0129308a29a197173c104874e612
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-07-17 14:43:42 +0330

    Cache device labels and preserve last selected option
    Bumped minor versions. Changed "no camera" icon to EyeDisabled

--- src/start-recording.tsx
+++ src/start-recording.tsx
@@ -1,21 +1,43 @@
-import { Action, ActionPanel, Form, Icon } from "@raycast/api";
+import { Action, ActionPanel, Cache, Form, Icon } from "@raycast/api";
 import { useEffect, useState } from "react";
 import { resolveAudioInputDevices, resolveVideoInputDevices } from "swift:../swift/cap-extension-helper";
 import { launchCap } from "./utils";
 
-export default function Command() {
-  const [audioInputDevices, setAudioInputDevices] = useState<string[]>([]);
-  const [videoInputDevices, setVideoInputDevices] = useState<string[]>([]);
+const AV_LABEL_NONE = "none";
+const AV_LABELS_CACHE_KEY = "av_device_labels";
 
+type AVDeviceList = {
+  audio: string[];
+  video: string[];
+};
+
+export default function Command() {
+  const cache = new Cache();
+  const cachedDevices = cache.get(AV_LABELS_CACHE_KEY);
+
+  const [devices, setDevices] = useState<AVDeviceList>(
+    cachedDevices
+      ? JSON.parse(cachedDevices)
+      : {
+          audio: [],
+          video: [],
+        },
+  );
   const [isFetchingDevices, setIsFetchingDevices] = useState(true);
-  const [selectedAudioDevice, setSelectedAudioDevice] = useState<string>("none");
-  const [selectedVideoDevice, setSelectedVideoDevice] = useState<string>("none");
+  const [selectedAudioDeviceLabel, setSelectedAudioDeviceLabel] = useState(AV_LABEL_NONE);
+  const [selectedVideoDeviceLabel, setSelectedVideoDeviceLabel] = useState(AV_LABEL_NONE);
 
   useEffect(() => {
     Promise.all([resolveAudioInputDevices(), resolveVideoInputDevices()]).then(([audioDevices, videoDevices]) => {
-      setAudioInputDevices(audioDevices);
-      setVideoInputDevices(videoDevices);
       setIsFetchingDevices(false);
+      setDevices({ audio: audioDevices, video: videoDevices });
+      cache.set(
+        AV_LABELS_CACHE_KEY,
+        JSON.stringify({
+          audio: [...audioDevices],
+          video: [...videoDevices],
+        }),
+      );
     });
   }, []);
 
@@ -28,31 +50,41 @@ export default function Command() {
           <Action.SubmitForm
             title="Start Recording"
             icon={Icon.Camera}
-            onSubmit={(_) =>
+            onSubmit={(_) => {
               launchCap("start-recording", {
                 params: {
-                  mic_in_label: selectedAudioDevice,
-                  vid_in_label: selectedVideoDevice,
+                  mic_in_label: selectedAudioDeviceLabel,
+                  vid_in_label: selectedVideoDeviceLabel,
                 },
                 feedbackMessage: "Launching Cap…",
-              })
-            }
+              });
+            }}
           />
         </ActionPanel>
       }
     >
-      <Form.Dropdown id="selected-audio" title="Audio Device" onChange={(device) => setSelectedAudioDevice(device)}>
-        <Form.Dropdown.Item value="none" title="No Audio" key={"none"} icon={Icon.MicrophoneDisabled} />
+      <Form.Dropdown
+        id="selected-audio"
+        title="Audio Device"
+        storeValue={true}
+        onChange={(device) => setSelectedAudioDeviceLabel(device)}
+      >
+        <Form.Dropdown.Item value={AV_LABEL_NONE} title="No Audio" key={AV_LABEL_NONE} icon={Icon.MicrophoneDisabled} />
 
-        {audioInputDevices.map((device) => (
+        {devices.audio.map((device) => (
           <Form.Dropdown.Item value={device} title={device} key={device} />
         ))}
       </Form.Dropdown>
 
-      <Form.Dropdown id="selected-video" title="Video Device" onChange={(device) => setSelectedVideoDevice(device)}>
-        <Form.Dropdown.Item value="none" title="No Camera" key={"none"} icon={Icon.CircleDisabled} />
+      <Form.Dropdown
+        id="selected-video"
+        title="Video Device"
+        storeValue={true}
+        onChange={(device) => setSelectedVideoDeviceLabel(device)}
+      >
+        <Form.Dropdown.Item value={AV_LABEL_NONE} title="No Camera" key={AV_LABEL_NONE} icon={Icon.EyeDisabled} />
 
-        {videoInputDevices.map((device) => (
+        {devices.video.map((device) => (
           <Form.Dropdown.Item value={device} title={device} key={device} />
         ))}
       </Form.Dropdown>
